0888a9b3ffc3773a8988c62117b65cfee9e6a491
Abdulbasit <basitsulyman@gmail.com>
2022-12-08 15:14:28 +0100

    CHANGES

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     return (
       <div className="App">
         
-         <h1> Covid19 App in React</h1>
+         <h1> Covid19 App in React 2022 VERSION</h1>
         <CountriesDropDown />
 
         <h1>Covid19 App in React MyApp 2022 VERSION</h1> 
